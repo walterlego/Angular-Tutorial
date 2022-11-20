037a040c87d4f43952371b5f80db62f7463c01a9
walterlego <47058753+walterlego@users.noreply.github.com>
2022-11-20 20:20:03 +0100

    Notification hinzugefügt

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,9 @@ export class ProductListComponent {
   share() {
     window.alert('The product has not been shared!');
   }
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale')
+  }
 }
 
 
